54c8dc4f25aa867b2f128027492d204ed08f8aa9
Zack <CrowsVeldt@users.noreply.github.com>
2023-07-19 19:49:54 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3001;
 app.get("/", (req, res) => res.type('html').send(html));
 
 app.get("/test", (req, res) => {
-  res.send('shit')  
+  res.send('stuff')  
 });
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
